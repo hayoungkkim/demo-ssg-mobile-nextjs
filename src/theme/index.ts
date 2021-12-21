8234301ecd84a3192f8b9629d99e751eef9db25b
hayoungkkim <glorious_3@naver.com>
2021-12-22 05:58:16 +0900

    오타 수정

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -3,17 +3,17 @@ import { createBreakpoints } from "@chakra-ui/theme-tools";
 import styles from "./styles";
 
 const breakpoints = createBreakpoints({
-	sm: "40em",
-	md: "52em",
-	lg: "64em",
-	xl: "80em",
+	sm: "40em", // 640
+	md: "52em", // 832
+	lg: "64em", // 1,024
+	xl: "80em", // 1,280
 });
 
 const theme = extendTheme({
 	styles,
 	fonts: {
 		heading: `'Roboto', 'AppleSDGothicNeo-Regular', sans-serif`,
-		body: `'Roboto', 'AppleSDGothicNeo-Regular', sans-seri`,
+		body: `'Roboto', 'AppleSDGothicNeo-Regular', sans-serif`,
 	},
 	breakpoints,
 });
